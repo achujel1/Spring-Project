96d880f5f20788bc84aa14ccd65fda8f5da56bbc
achujel1 <achujel1@gmail.com>
2015-06-06 10:16:16 +0300

    Part 6. Using Constructor Injection
    In this part I've tested constructor injection

--- src/org/stuff/Triangle.java
+++ src/org/stuff/Triangle.java
@@ -9,20 +9,56 @@ package org.stuff;
 public class Triangle {
 
 	private String type;
+	private int height;
+
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * Constructor with two arguments
+	 * 
+	 * @param type
+	 * @param height
+	 */
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+
+	/**
+	 * Constructor with one argument
+	 * 
+	 * @param type
+	 */
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * Constructor with one argument
+	 * 
+	 * @param height
+	 */
+	public Triangle(int height) {
+		this.height = height;
+	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
+	// Commenting a setter to show that constructor with argument is working
+	// public void setType(String type) {
+	// this.type = type;
+	// }
 
 	/**
 	 * Method which is drawing a triangle
 	 */
 	public void draw() {
-		System.out.println(getType() + " triangle drawn");
+		System.out.println(getType() + " triangle drawn with height "
+				+ getHeight());
 	}
 
 }
