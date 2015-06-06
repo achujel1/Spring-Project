cbe7f04cf011876b8005b4bb93653fbd63e9c0e3
achujel1 <achujel1@gmail.com>
2015-06-06 09:10:53 +0300

    Small changes

--- src/org/stuff/Triangle.java
+++ src/org/stuff/Triangle.java
@@ -1,7 +1,16 @@
 package org.stuff;
 
+/**
+ * Triangle class
+ * 
+ * @author Adminas
+ *
+ */
 public class Triangle {
 
+	/**
+	 * Method which is drawing a triangle
+	 */
 	public void draw() {
 		System.out.println("Triangle drawn");
 	}
