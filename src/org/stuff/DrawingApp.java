912befb9f02ac43e04b72a2e4cdbc553fbbd0aeb
achujel1 <achujel1@gmail.com>
2015-06-12 20:45:30 +0300

    Part 13. Bean definition inheritance
    Worked with bean inheritance:
    - Created a beans that inherited from parent bean
    - Created a bean which inherited a list from parent bean
    - Tested how everything worked

--- src/org/stuff/DrawingApp.java
+++ src/org/stuff/DrawingApp.java
@@ -3,6 +3,7 @@
  */
 package org.stuff;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,18 +14,59 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DrawingApp {
 
 	/**
+	 * Main method to write tests in
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// nothing in here
+	}
+
+	/**
+	 * Just a method to test how bean inheritance with lists work
+	 * 
+	 * @throws BeansException
+	 */
+	private static void workingWithBeanListInheritance() throws BeansException {
 		// Gets the resource
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"spring.xml");
 
-		Triangle triangle = (Triangle) context.getBean("triangle");
+		Triangle triangle = (Triangle) context.getBean("triangle3");
+
+		triangle.drawPoints();
+
+		// Closes the context
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+	/**
+	 * List of tested methods to keep in here
+	 */
+	private static void testedMethods() {
+		testingBeanInheritance();
+		workingWithBeanListInheritance();
+	}
+
+	/**
+	 * Testing bean definition inheritance
+	 */
+	private static void testingBeanInheritance() {
+		// Gets the resource
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring.xml");
+
+		// Testing how this is going to work when we add inheritance
+		Triangle triangle = (Triangle) context.getBean("triangle1");
+
+		// Now testing how this is going to work with another bean
+		// Triangle triangle = (Triangle) context.getBean("triangle2");
+
 		triangle.drawPoint();
 
 		// Closes the context
 		((ClassPathXmlApplicationContext) context).close();
+
 	}
 
 }
